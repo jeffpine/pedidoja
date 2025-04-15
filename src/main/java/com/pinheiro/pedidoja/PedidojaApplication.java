be755283aefdfedd681edddddce90b21315c9965
Jeferson Pinheiro de Jesus <65078428+jeffpine@users.noreply.github.com>
2025-04-15 14:32:35 -0300

    Add files via upload
    my first commit

---
+++ src/main/java/com/pinheiro/pedidoja/PedidojaApplication.java
@@ -0,0 +1,13 @@
+package com.pinheiro.pedidoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinheiro/pedidoja/PedidojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinheiro.pedidoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
